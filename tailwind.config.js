e654fd0dec891328c2f68b5b60d66fee0d8e6097
muhammadashiqurrahman <19103006@iubat.edu>
2023-06-08 15:31:37 +0600

    tailwind package update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,50 +2,45 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    "./pages/**/*.{html,js}",
-    "./**/*.{html,js}",
-  ],
+    "{html,js}", './index.html', './aboutus.html'
+  ], 
   theme: {
     fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
+      'poppins': ['Poppins', 'sans-serif'],
+      'body': ['"Roboto"'],
     },
-    extend: {
-      colors: {
-        themeBlack: "#334155",
-        lightBlue: "#10B981",
-        gary: "#e2e8f0",
-        lightGary: "#F1F5F9",
-        green: "#10B981",
-        greenLight: "#D1FAE5",
-        greenDeep: "#047857",
-        grayLight: "#CBD5E1",
-        yellow: "#FEF3C7",
-        yellowLight: "#F59E0B",
-        yellowDeep: "#B45309",
-        light: "#F1F5F9",
-        lightLight: "#64748B",
-        lightDeep: "#334155",
-        redLight: "#FEE2E2",
-        red: "#EF4444",
-        redDeep: "#B91C1C",
 
-        // dark color
-        dark: "#1E293B",
-        lightDark: "#6c7a87",
-        lightBlack: "#e8eaec",
-        lightWhite: "#F8FAFC",
-        lightGreen: "#E7F8F2",
+
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1110px',
+    },
+
+
+    extend: {
+
+      colors: {
+        //Primary colors
+        blue: "#0078D2",
+        main_dark: "#0B0140",
+        main_light: "#F5F3FD",
+
+        //Secondary Color
+        secondary: "#4AF8E3",
+        secondary_shade: "#FFF3EE",
+        secondary_light: "#F3EADA",
+
+        //Dark Color
+        dark1: "#06021C",
+        dark2: "#33313E",
+
+
       },
-      fontSize: {
-        base: "16px",
-        md: "18px",
-        lg: "20px",
-        xl: "24px",
-        xxl: "30px",
-        xxxl: "50px",
-      },
+
     },
   },
   plugins: [],
-};
+}
